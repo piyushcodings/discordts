7d5a9e226b8114ca9a1e72e179d6d16ef9ec72fb
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-08-28 07:13:50 +0530

    Create button.py

---
+++ online/helpers/button.py
@@ -0,0 +1,17 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="Developer",
+                url="https://t.me/LegendBot_Owner",
+            ),
+            InlineKeyboardButton(
+                text="About Me",
+                url="https://t.me/LegendBot_AI/476",
+            ),
+        ],
+    ]
+)
