aec542490a129c8360e08c3d3e69f87b28122a40
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-08-28 06:54:02 +0530

    Update and rename details.py to sampl_config.py

---
+++ sampl_config.py
@@ -0,0 +1,7 @@
+import os
+
+api_id = 3748059
+api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
+bot_token = os.environ.get("BOT_TOKEN")
+auth_users = os.environ.get("AUTH_USERS", 5591734243)
+owner_users = os.environ.get("OWNER_USERS", 5591734243)
