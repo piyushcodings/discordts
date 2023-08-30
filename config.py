98d2d4c98445d69764a82b8c4e8e535829f0beea
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-08-30 10:30:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 3748059
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6301605779:AAFcZLmRnChQKqGVenRhbTMCNFsgLvw_vH4"
+bot_token = "6379231147:AAG89J5scOJVvaj4OcyV3rcvVVxgWbHS6qY"
 auth_users = "870101201,5593976733,5297539026,5591734243,1369808729,1993514215,6161200827"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "5591734243,1369808729"
