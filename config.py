c8c9d8490a8b059357b415a9013a7b994bc95239
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2024-04-21 15:41:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 23907288
 api_hash = "f9a47570ed19aebf8eb0f0a5ec1111e5"
 bot_token = "7133197143:AAGGn76vxJyAxOcZa3E6iXtl6aiZSBiSF00"
-auth_users = ""
+auth_users = "6464246531,6464246531"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6464246531"
+osowner_users = "6464246531,6464246531"
 owner_users = [int(num) for num in osowner_users.split(",")]
